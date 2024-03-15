bcead70e7bdbdcaf7819c8759594f1e150d6edb8
Grady Peterson <grady@snand.org>
2024-03-14 20:35:39 -0500

    where did those " come from?

--- wordpress/genenv.sh
+++ wordpress/genenv.sh
@@ -13,50 +13,47 @@
 if [ ! -f ".env" ]; then
     echo ".env file does not exist, creating..."
     
-    # Generate random database password
+    # generate random database password
     MYSQL_DATABASE_PASSWORD=$(openssl rand -base64 36)
 
-    # Generate random database password
+    # generate random database password
     MYSQL_DATABASE_ROOT_PASSWORD=$(openssl rand -base64 48)
 
-    # Prompt user for Upload location
+    # prompt user for Upload location
     read -p "please enter the location for the databse (./db as default): " MYSQL_DATA
     MYSQL_DATA=${MYSQL_DATA:-'./db'}
 
-    # Prompt user for Upload location
+    # prompt user for Upload location
     read -p "please enter the location for the wordpress content (./config as default): " WORDPRESS_DATA
     WORDPRESS_DATA=${WORDPRESS_DATA:-'./config'}
 
-    # Prompt user for Immich URL
+    # prompt user for wordpress URL
     read -p "please provide the wordpress URL (default www.snand.org): " WORDPRESS_URL
     WORDPRESS_URL=${WORDPRESS_URL:-'www.snand.org'}
 
-    # Prompt user for Immich URL
-    read -p "please provide the Immich URL (default db.snand.org): " MYPHPADMIN_URL
+    # prompt user for myphpadmin URL
+    read -p "please provide the myphpadmin URL (default db.snand.org): " MYPHPADMIN_URL
     MYPHPADMIN_URL=${MYPHPADMIN_URL:-'db.snand.org'}
 
-    # Write all variables at once to the .env file
+    # write all variables at once to the .env file
     {
         echo "MYSQL_DATABASE_PASSWORD=$MYSQL_DATABASE_PASSWORD"
         echo "MYSQL_DATABASE_ROOT_PASSWORD=$MYSQL_DATABASE_ROOT_PASSWORD"
         echo "MYSQL_DATA=$MYSQL_DATA"
         echo "WORDPRESS_DATA=$WORDPRESS_DATA"
-        echo "IMMICH_VERSION=$IMMICH_VERSION"
-        echo "ENV_NAME=$ENV_NAME"
         echo "WORDPRESS_URL=$WORDPRESS_URL"
         echo "MYPHPADMIN_URL=$MYPHPADMIN_URL"
-        # The values below do not need to be changed
+        # the values below do not need to be changed
         echo "MYSQL_DATABASE_USER_NAME=wordpress"
         echo "WEB_SERVER_PORT=4321"
     } > .env
 
 else
     echo ".env file already exists, creating a backup and moving on..."
-    # Ensure backup directory exists
-    mkdir -p ./backup
 fi
-# Create a backup of the current .env file
-cp .env "./backup/env_$(date +%Y%m%d%H%M%S)"
-# Additional code that needs to run after this script
+# backup .env file - ensure backup directory exists
+mkdir -p ./backup
+cp .env "./backup/env_$(date +%Y%m%d)"
+# additional code that needs to run after this script
 echo "please run 'docker compose up -d' to start the application..."
 echo "then open a web browser to https://$WORDPRESS_URL"
