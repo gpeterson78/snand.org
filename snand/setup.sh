5f9eafa162ee92276c00e07206cbf216fb36fad8
Grady Peterson <grady@gradyp.com>
2024-09-02 12:27:35 -0500

    more try

--- snand/setup.sh
+++ snand/setup.sh
@@ -32,18 +32,6 @@ for SCRIPT in "genenv.sh" "wordpress_backup.sh" "wordpress_restore.sh"; do
         echo "$SCRIPT already exists in /snand/scripts, skipping download."
     fi
 done
-
-
-SCRIPTS=("genenv.sh" "wordpress_backup.sh" "wordpress_restore.sh")
-for SCRIPT in "${SCRIPTS[@]}"; do
-    if [ ! -f "/snand/scripts/$SCRIPT" ]; then
-        sudo wget -P /snand/scripts/ "https://raw.githubusercontent.com/gpeterson78/snand.org/main/snand/scripts/$SCRIPT"
-        sudo chmod +x "/snand/scripts/$SCRIPT"
-        echo "Downloaded and made $SCRIPT executable."
-    else
-        echo "$SCRIPT already exists in /snand/scripts, skipping download."
-    fi
-done
 # Download docker-compose.yml files if they don't already exist
 for COMPOSE_FILE in "/snand/docker/wordpress/docker-compose.yaml"; do
     if [ ! -f "$COMPOSE_FILE" ]; then
