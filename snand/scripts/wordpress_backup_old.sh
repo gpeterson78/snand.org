1222c7cf1f65aea08447729c0e8be5209cb50252
Grady Peterson <grady@gradyp.com>
2024-09-02 20:38:10 -0500

    cleaning up some old, unused passwords.

--- snand/scripts/wordpress_backup_old.sh
+++ snand/scripts/wordpress_backup_old.sh
@@ -6,6 +6,6 @@ set -a
 . ../.env
 set +a
 # Backup the WordPress database
-docker exec wordpressdb sh -c 'exec mariadb-dump -u"wordpress" -p"fstLOLDM59nOHAyYplR1bclBt5oqmERG31TCUW0vf3mZTUiH" wordpress' | gzip > wp_backup_$(date +%Y%m%d).sql.gz
+docker exec wordpressdb sh -c 'exec mariadb-dump -u"wordpress" -p"--redacted--" wordpress' | gzip > wp_backup_$(date +%Y%m%d).sql.gz
 
-docker exec wordpressdb sh -c "exec mariadb-dump -u'wordpress' -p'fstLOLDM59nOHAyYplR1bclBt5oqmERG31TCUW0vf3mZTUiH' wordpress" | gzip > "wp_backup_$(date +%Y%m%d).sql.gz"
+docker exec wordpressdb sh -c "exec mariadb-dump -u'wordpress' -p'--redacted--' wordpress" | gzip > "wp_backup_$(date +%Y%m%d).sql.gz"
